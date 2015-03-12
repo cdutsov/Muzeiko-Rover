fb7ec849e85b7260377b31162ec6c94190ad74fe
Georgi Georgiev <georgiev.georgi.st@gmail.com>
2015-03-12 18:57:54 +0200

    Initial commit for robot_UI.sh
    This is a user interface script which sends to the RS232 start and stop
    doing something commands.

---
+++ robot_UI.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+
+#DEVICE=/dev/ttyUSB0
+DEVICE=/tmp/dev
+
+START_FORWARD_CMD="w"
+START_BACKWORD_CMD="s"
+START_TURNING_LEFT_CMD="a"
+START_TURNING_RIGHT_CMD="d"
+
+
+STOP_FORWARD_CMD="W"
+STOP_BACKWORD_CMD="S"
+STOP_TURNING_LEFT_CMD="A"
+STOP_TURNING_RIGHT_CMD="D"
+
+
+
+EOC_i=1;
+for ((;;));
+do
+  cmd=""
+  read -t 1 -sn1 cmd &&{
+    EOC_i=0;
+    [ x"$cmd" != x"$cmdold" ] && {
+      #echo was "$cmdold" now "$cmd"
+      case $cmd in
+        w) echo forward;  echo $START_FORWARD_CMD    > $DEVICE ;;
+        s) echo backword; echo $START_BACKWORD_CMD   > $DEVICE ;;
+        a) echo left; echo $START_TURNING_LEFT_CMD   > $DEVICE ;;
+        d) echo right; echo $START_TURNING_RIGHT_CMD > $DEVICE ;;
+        *) echo UNKNOWN COMMAND $cmd ;;
+      esac
+    }
+    cmdold=$cmd;
+  }||{
+    cmd="";
+    ((EOC_i++<1))&&{
+      #echo end of command $cmdold;
+      case $cmdold in
+        w) echo stop forward ; echo $STOP_FORWARD_CMD   > $DEVICE ;;
+        s) echo stop backword; echo $STOP_BACKWORD_CMD  > $DEVICE ;;
+        a) echo stop left ;echo $STOP_TURNING_LEFT_CMD  > $DEVICE ;;
+        d) echo stop right;echo $STOP_TURNING_RIGHT_CMD > $DEVICE ;;
+        #*) echo UNKNOWN COMMAND $cmd ;;
+      esac
+      cmdold=""
+      }
+  }
+done
